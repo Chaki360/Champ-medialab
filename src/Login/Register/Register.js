8866e3ccacb245c8cc9c8027ac3ef43396478516
Badhon chaki <fboy4069@gmail.com>
2022-04-19 16:51:48 +0600

    servicedata added

--- src/Login/Register/Register.js
+++ src/Login/Register/Register.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Register.css'
 const Register = () => {
+
+    const navigate = useNavigate()
+    const navigateLogin = e => {
+        navigate('/login')
+    }
     return (
         <div className='register-form'>
             <h2 className='text-5xl mb-4 text-center text-bold text-orange-600'>Register Now</h2>
@@ -10,6 +16,7 @@ const Register = () => {
                 <input className='border' type="password" name="password" id="" placeholder="Enter your password" required />
                 <input className='rounded text-center text-white bg-orange-500 hover:bg-orange-700' type="register" value="Register to your account" />
             </form>
+            <h1 className='text-lg'>Already have an account?<span className=' text-red-500 hover:text-gray-700' onClick={navigateLogin}>Please Login</span></h1>
         </div>
     );
 };
